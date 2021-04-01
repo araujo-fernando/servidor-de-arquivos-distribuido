c469c82656d93501fb94a573faf31c1e4bce2e5e
Fernando <23432382ferjnr@users.noreply.github.com>
2021-04-01 08:24:08 -0300

    importação inicial

---
+++ executar.bat
@@ -0,0 +1,11 @@
+@ECHO OFF
+cd C:\Users\Fernando\Desktop\UFMG\2020_2\[RC]-Redes_de_Computadores\tp3\
+start cmd /k python peer.py 127.0.0.1:60001 keys1.txt 127.0.0.3:60003 127.0.0.2:60002
+start cmd /k python peer.py 127.0.0.2:60002 keys2.txt 127.0.0.1:60001 127.0.0.5:60005
+start cmd /k python peer.py 127.0.0.3:60003 keys3.txt 127.0.0.1:60001 127.0.0.5:60005
+start cmd /k python peer.py 127.0.0.4:60004 keys4.txt 127.0.0.5:60005
+start cmd /k python peer.py 127.0.0.5:60005 keys5.txt 127.0.0.2:60002 127.0.0.3:60003 127.0.0.4:60004
+timeout /t 10 /nobreak > NUL
+start cmd /k python cliente.py 127.0.0.1:60001 1,3,2,4,5
+timeout /t 75 /nobreak > NUL
+start cmd /k python cliente.py 127.0.0.1:60001 1,3,2,5,4,6,9
